522955c508399dd356931f8a94f51d8d08c62066
草鞋没号 <308487730@qq.com>
2022-11-14 11:29:00 +0800

    refactor!: remove `worker()`, remove Node.js API support.

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import type { PluginOption, Plugin } from 'vite'
 import buildConfig from './build-config'
 import cjsShim from './cjs-shim'
 import {
-  type UseNodeJsOptions,
-  default as useNodeJs,
-} from './use-node.js'
-import {
   type DepOptimizationConfig,
   default as optimizer,
 } from './optimizer'
 
 export default function renderer(
-  options: Omit<UseNodeJsOptions, 'nodeIntegrationInWorker'> & {
+  options: {
+    /**
+     * Whether node integration is enabled. Default is `false`.
+     */
+    nodeIntegration?: boolean
     /**
      * If the npm-package you are using is a Node.js package, then you need to Pre Bundling it.
      * @see https://vitejs.dev/guide/dep-pre-bundling.html
@@ -22,15 +22,6 @@ export default function renderer(
   return [
     buildConfig(),
     options.nodeIntegration && cjsShim(),
-    useNodeJs(options),
     options.optimizeDeps && optimizer(options.optimizeDeps),
   ]
-}
-
-export function worker(
-  options: Omit<UseNodeJsOptions, 'nodeIntegration'> = {}
-): Plugin[] {
-  // TODO: Tree-shake after worker build
-  return useNodeJs(options)
-    .map(plugin => Object.assign(plugin, { api: { isWorker: true } }))
 }
