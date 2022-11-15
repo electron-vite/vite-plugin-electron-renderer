c9b42beb9a901f6fe9f67200279bdfc3aeaa2a03
草鞋没号 <308487730@qq.com>
2022-11-15 08:49:27 +0800

    refactor: build with Vite

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import { builtinModules } from 'module'
+import pkg from './package.json'
+
+export default defineConfig({
+  build: {
+    minify: false,
+    emptyOutDir: false,
+    outDir: '',
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['cjs', 'es'],
+      fileName: format => format === 'es' ? '[name].mjs' : '[name].js',
+    },
+    rollupOptions: {
+      external: [
+        'esbuild',
+        'vite',
+        ...builtinModules,
+        ...builtinModules.map(m => `node:${m}`),
+        ...Object.keys(pkg.dependencies),
+      ],
+      output: {
+        exports: 'named',
+      },
+    },
+  },
+})
