e9c31aed27e3aee704a316775cdb057f4f86776d
草鞋没号 <308487730@qq.com>
2022-11-15 08:51:19 +0800

    chore(examples): update to v0.11.0

--- examples/quick-start/renderer/samples/node.js-api.ts
+++ examples/quick-start/renderer/samples/node.js-api.ts
@@ -1,5 +1,5 @@
 import { ipcRenderer } from 'electron'
 import fs from 'fs'
 
-console.log('Node.js API:\n', ipcRenderer)
+console.log('Electron API:\n', ipcRenderer)
 console.log('Node.js API:\n', fs)
