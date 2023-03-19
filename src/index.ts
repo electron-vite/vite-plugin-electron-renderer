485a516041726a25273c6751c68391af431778e3
草鞋没号 <308487730@qq.com>
2023-03-19 20:46:09 +0800

    feat!: Pre-Bundling modules for Electron Renderer process

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import type { PluginOption } from 'vite'
 import buildConfig from './build-config'
 import cjsShim from './cjs-shim'
-import optimizer, { type DepOptimizationOptions } from './optimizer'
+import {
+  type optimizerOptions,
+  default as optimizer,
+} from './optimizer'
 
 export default function renderer(
   options: {
@@ -10,15 +13,18 @@ export default function renderer(
      */
     nodeIntegration?: boolean
     /**
-     * If the npm-package you are using is a Node.js package, then you need to Pre-Bundling it.
-     * @see https://vitejs.dev/guide/dep-pre-bundling.html
+     * Pre-Bundling modules for Electron Renderer process.
      */
-    optimizeDeps?: DepOptimizationOptions
+    optimizer?: optimizerOptions
   } = {}
 ): PluginOption {
+  const {
+    nodeIntegration = false,
+    optimizer: optimizerOpts = {},
+  } = options
   return [
     buildConfig(options.nodeIntegration),
-    optimizer(options.optimizeDeps),
-    options.nodeIntegration && cjsShim(),
+    optimizer(optimizerOpts, nodeIntegration),
+    nodeIntegration && cjsShim(),
   ]
 }
