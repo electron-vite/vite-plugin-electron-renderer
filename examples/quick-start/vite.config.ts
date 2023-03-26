6362b8a7e9f1abf492620e53cd7e8ff8b39755af
草鞋没号 <308487730@qq.com>
2023-03-26 16:34:55 +0800

    chore(examples): update quick-start

--- examples/quick-start/vite.config.ts
+++ examples/quick-start/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
-import pkg from './package.json'
 
 export default defineConfig({
   plugins: [
@@ -9,22 +8,13 @@ export default defineConfig({
       entry: 'electron/main.ts',
     }),
     renderer({
-      // Enables use of Node.js API in the Renderer-process
-      nodeIntegration: true,
-      optimizeDeps: {
-        resolve(args) {
-          if (args.path === 'serialport') {
-            return { platform: 'node' }
-          }
-        },
+      resolve: {
+        serialport: () => ({ platform: 'node' }),
       },
     }),
   ],
   build: {
     minify: false,
-    rollupOptions: {
-      external: Object.keys(pkg.dependencies),
-    },
   },
   optimizeDeps: {
     // If an npm package is a pure ESM format package, 
